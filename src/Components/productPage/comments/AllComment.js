cea215b01aa796b9766fdc6173788ccb68427fd7
Erfan Hoseini <s.erfan.hoseini.81@gmail.com>
2022-05-29 14:29:32 +0430

    update style sliders update style comment update fetching profile delete PurchaseGuide update hover buttons

--- src/Components/productPage/comments/AllComment.js
+++ src/Components/productPage/comments/AllComment.js
@@ -1,22 +1,20 @@
 import React, { useLayoutEffect, useState } from "react";
 import Comment from "./Comment";
-// import "owl.carousel/dist/assets/owl.carousel.css";
-// import "owl.carousel/dist/assets/owl.theme.default.css";
-import dynamic from "next/dynamic";
 import Style from "./Comments.module.css";
 import AddComment from "./AddComment";
 import WriteComment from "./WriteComment";
 import { useDispatch, useSelector } from "react-redux";
 import { writeTrue } from "../../../redux/comment/commentActions";
-// var $ = require("jquery");
-// if (typeof window !== "undefined") {
-//   // Client-side-only code
-//   window.$ = window.jQuery = require("jquery");
-// }
 
-// const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
-//   ssr: false
-// });
+
+//swiper lib
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCreative, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/effect-creative";
+
+
+
 const AllComment = () => {
   const state = useSelector((state) => state.stateComment);
   const [size, setSize] = useState([0]);
@@ -45,29 +43,60 @@ const AllComment = () => {
 
   return (
     <>
-      {size < 600 && <AddComment />}
-      <div className={`${Style.AllComment} mt-2`}>
-        {/* <OwlCarousel
-          responsive={respons.responsive}
-          margin={16}
-          startPosition={10}
-          autoWidth={size < 650 ? false : true}
-          dir={"ltr"}
-          mouseDrag={size > 650&&state.writeCm ? false : true}
-          touchDrag={size > 650&&state.writeCm ? false : true}
-        > */}
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
-        <Comment />
-        {size > 600 && <AddComment />}
-        {/* </OwlCarousel> */}
+      {/* {size < 600 && <AddComment />} */}
+      <div className={`${Style.AllComment} row`}>
+        {/* <div className="row"> */}
+          <div className="col-12 col-md-6 p-1">
+              <Swiper
+                    pagination={{
+                      clickable: true,
+                    }}
+                    grabCursor={true}
+                    effect={"creative"}
+                    creativeEffect={{
+                      prev: {
+                        shadow: true,
+                        origin: "left center",
+                        translate: ["-5%", 0, -200],
+                        rotate: [0, 100, 0],
+                      },
+                      next: {
+                        origin: "right center",
+                        translate: ["5%", 0, -200],
+                        rotate: [0, -100, 0],
+                      },
+                    }}
+                    modules={[EffectCreative, Pagination]}
+                    className="mySwiperComment"
+              >
+                    <SwiperSlide> 
+                        <Comment />
+                    </SwiperSlide>
+                    <SwiperSlide> 
+                        <Comment />
+                    </SwiperSlide>
+                    <SwiperSlide> 
+                        <Comment />
+                    </SwiperSlide>
+                    <SwiperSlide> 
+                        <Comment />
+                    </SwiperSlide>
+                    <SwiperSlide> 
+                        <Comment />
+                    </SwiperSlide>
+                    <SwiperSlide> 
+                        <Comment />
+                    </SwiperSlide>
+                    <SwiperSlide> 
+                        <Comment />
+                    </SwiperSlide>
+                </Swiper>
+          </div>
+          <div className="col-12 col-md-6 p-1">
+            <AddComment />
+          </div>
+        {/* </div> */}
+        
       </div>
     </>
   );
