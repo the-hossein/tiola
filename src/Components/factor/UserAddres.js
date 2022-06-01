07628537a54de90c05df9150f382146fbc294a4b
fatemehdaryan <baridaryan@gmail.com>
2022-06-01 22:13:36 +0430

    add addres api

--- src/Components/factor/UserAddres.js
+++ src/Components/factor/UserAddres.js
@@ -5,19 +5,57 @@ import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 import { useDispatch, useSelector } from "react-redux";
-import { checkAddress, getAddres } from "../../redux/factor/factorAction";
+import {
+  checkAddress,
+  getAddres,
+  getuserAddress
+} from "../../redux/factor/factorAction";
 import Loader from "../../tools/loader/Loader";
+import callApi from "../../api/callApi";
+import { ADD_ADDRESS, BASE_URL } from "../../api/urls";
 const UserAddres = () => {
   const dispatch = useDispatch();
   const [checked, setChecked] = useState(false);
   const [edit, setEdit] = useState(false);
   const [address, setAddress] = useState();
-const state=useSelector(state=>state.stateFactor)
+  const state = useSelector((state) => state.stateFactor);
+  const user = useSelector((state) => state.stateRegister);
+
   const changeRadio = (e) => {
     dispatch(getAddres(document.getElementById(`${e.target.value}`).value));
     dispatch(checkAddress(e.target.value));
   };
-  const addAddres = (e) => {};
+  if (typeof window !== "undefined") {
+    var ls = localStorage.getItem("userToken");
+  }
+
+  const addAddres = (e) => {
+    const userToken = JSON.parse(ls);
+    const token = userToken.token;
+
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${token}`);
+    myHeaders.append("Content-Type", "application/json");
+    console.log(state.addres);
+    var raw = JSON.stringify({
+      id: user.basketid,
+      address: `${state.addres}`,
+      userid: user.userid
+    });
+    const addAddresUser = async () => {
+      console.log(raw);
+      const addStatus = await callApi(
+        BASE_URL + ADD_ADDRESS,
+        raw,
+        myHeaders,
+        "POST"
+      );
+      if (addStatus[0].code === 200) {
+        dispatch(getuserAddress(user.userid));
+      }
+    };
+    addAddresUser();
+  };
   return (
     <div className="row m-0">
       <div className={`col-lg-6 col-md-6 col-12 overflow-hidden ${style.map}`}>
@@ -27,10 +65,11 @@ const state=useSelector(state=>state.stateFactor)
         {state.loading ? (
           <Loader />
         ) : (
-          state.allAddress.map((item) => {
+          state.allAddress.map((item) => (
             <>
               <AddresInput
-                id="addres1"
+                data={item}
+                id={item.id}
                 checkicon={<CheckBoxIcon sx={{ fontSize: 30 }} />}
                 icon={
                   <CheckBoxOutlineBlankIcon
@@ -39,11 +78,12 @@ const state=useSelector(state=>state.stateFactor)
                 }
                 onChangeRadio={(e) => changeRadio(e)}
               />
-            </>;
-          })
+            </>
+          ))
         )}
 
         <AddresInput
+          data="addAddress"
           id="add"
           checkicon={
             <AddBoxOutlinedIcon sx={{ fontSize: 30, color: "#b5b5b5" }} />
--- src/Components/factor/addres/AddresInput.js
+++ src/Components/factor/addres/AddresInput.js
@@ -14,17 +14,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAddres } from "../../../redux/factor/factorAction";
 
 //make style for components mui
-import { makeStyles } from '@mui/styles';  
+import { makeStyles } from "@mui/styles";
+import callApi from "../../../api/callApi";
+import { BASE_URL, DELETE_ADDRESS } from "../../../api/urls";
 
 const useStyle = makeStyles({
   applyFont: {
     fontFamily: "var(--font) !important"
   }
-})
+});
 
-
-
-const AddresInput = ({ id, checkicon, icon, onChangeRadio ,type}) => {
+const AddresInput = ({ data, id, checkicon, icon, onChangeRadio, type }) => {
+  console.log(data);
   //useStyle for stylesheet mui
   const classes = useStyle();
 
@@ -36,62 +37,92 @@ const AddresInput = ({ id, checkicon, icon, onChangeRadio ,type}) => {
   const [checked, setChecked] = useState(false);
   const [edit, setEdit] = useState(false);
   const [address, setAddress] = useState();
-  const [addresInput, setAddresInput] = useState("City1-street1-num1-unit-1");
+  // const [addresInput, setAddresInput] = useState("City1-street1-num1-unit-1");
   const [deleteAddres, setDeleteAddres] = useState(false);
-  const changeAddres = (e) => {
-    setAddresInput(e.target.value);
-  };
-  return !deleteAddres ? (
-    <div>
-      {type !== "add" ? (
-        <>
-          <EditBtn onclick={() => setEdit(true)} text={t("edit")} />
-          <EditBtn
-            onclick={() => setDeleteAddres(true)}
-            text={<DeleteIcon sx={{ fontSize: 17 }} />}
-          />
-        </>
-      ):null}
-      <Box
-        component="form"
-        noValidate
-        autoComplete="off"
-        className={Style.inputContainer}
-      >
+  if (typeof window !== "undefined") {
+    var root = document.documentElement;
+    var ls = localStorage.getItem("userToken");
+    var userToken = JSON.parse(ls);
 
-        <FormControl sx={{ width: "100%",position:"relative" }} className={classes.applyFont}>
-          <OutlinedInput
-            onChange={changeAddres}
-            placeholder={
-              id === "add" ? t("addCmPlaceHOlder") : t("addresPlaceHolder")
-            }
-            value={addresInput}
-            readOnly={edit ? false : true}
-            multiline={true}
-            style={
-              edit
-                ? { backgroundColor: "#ffffff", color: "#000000" }
-                : { backgroundColor: "#ffffff", color: "#707070" }
-            }
-            className={`${Style.addresinput} ${classes.applyFont}`}
-            id={id}
-          />
-        <Radio
-          checkedIcon={checkicon}
-          icon={icon}
-          checked={state.check === id}
-          onChange={onChangeRadio}
-          value={id}
-          name="radio-buttons"
-          inputProps={{ "aria-label": `${id}` }}
-          className={lang.lng === "en" ? Style.checkedRight : Style.checkedLeft}
-        />
-        </FormControl>
-      </Box>
-    </div>
-  ) : (
-    <></>
-  );
+    var token = userToken.token;
+  }
+  const changeAddres = (e) => {
+    dispatch(getAddres(e.target.value));
+    console.log(state);
+  };
+  const deleteAddress = () => {
+    const deleted = async () => {
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${token}`);
+      const status = await callApi(
+        `${BASE_URL + DELETE_ADDRESS}?id=${data.id}`,
+        "{}",
+        myHeaders,
+        "POST"
+      );
+     if(status[0].code===200){
+      setDeleteAddres(true)
+     }
+    };
+    deleted();
+  };
+  if (typeof data !== "undefined") {
+    return !deleteAddres ? (
+      <div>
+        {type !== "add" ? (
+          <>
+            <EditBtn onclick={() => setEdit(true)} text={t("edit")} />
+            <EditBtn
+              onclick={ deleteAddress}
+              text={<DeleteIcon sx={{ fontSize: 17 }} />}
+            />
+          </>
+        ) : null}
+        <Box
+          component="form"
+          noValidate
+          autoComplete="off"
+          className={Style.inputContainer}
+        >
+          <FormControl
+            sx={{ width: "100%", position: "relative" }}
+            className={classes.applyFont}
+          >
+            <OutlinedInput
+              onChange={changeAddres}
+              placeholder={
+                type === "add" ? t("addCmPlaceHOlder") : t("addresPlaceHolder")
+              }
+              value={data.address}
+              readOnly={type === "add" ? false : edit ? false : true}
+              multiline={true}
+              style={
+                edit
+                  ? { backgroundColor: "#ffffff", color: "#000000" }
+                  : { backgroundColor: "#ffffff", color: "#707070" }
+              }
+              className={`${Style.addresinput} ${classes.applyFont}`}
+              id={id}
+            />
+            <Radio
+              checkedIcon={checkicon}
+              icon={icon}
+              checked={type!=="add"&&data.id === id}
+              onChange={onChangeRadio}
+              value={id}
+              name="radio-buttons"
+              inputProps={{ "aria-label": `${id}` }}
+              className={
+                lang.lng === "en" ? Style.checkedRight : Style.checkedLeft
+              }
+            />
+          </FormControl>
+        </Box>
+      </div>
+    ) : (
+      <></>
+    );
+  }
 };
 
 export default AddresInput;
