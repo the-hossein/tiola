3fb330a1a241b8d8d55568a35755ea96239ed0a7
fatemehdaryan <baridaryan@gmail.com>
2022-06-01 22:18:11 +0430

    add addres api and get address

---
+++ src/Components/landing/Landing.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Collection from "../collection/Collection";
+import HeroSection from "../heroSection/HeroSection";
+import ProductSlider from "../productSlider/ProductSlider";
+import style from "./Landing.module.css";
+const Landing = () => {
+    const product = [
+        {
+          src: "/Assets/images/1.jpg"
+        },
+        {
+          src: "/Assets/images/1.jpg"
+        },
+        {
+          src: "/Assets/images/1.jpg"
+        }
+     
+      ];
+  return (
+    <div className={style.landing}>
+      <HeroSection />
+      <ProductSlider
+        slidesShow={3}
+        radius={0}
+        images={product}
+        arrowStatus={true}
+        margin={0}
+        mbItem={1}
+        tbItem={2}
+        heightImage={"auto"}
+      />
+      <Collection />
+    </div>
+  );
+};
+
+export default Landing;
