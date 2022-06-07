91e5411a6382e42e2cdf064f53b9addc8256cff7
fatemehdaryan <baridaryan@gmail.com>
2022-06-07 11:27:52 +0430

    fixed pay when no addres

--- src/Components/factor/buylist/BuyLists.js
+++ src/Components/factor/buylist/BuyLists.js
@@ -64,7 +64,16 @@ const BuyLists = ({ setBasketDatas }) => {
         if (status[0].code === 200) {
           setpreloadPay(false);
           window.location = status[0].data.requestBank;
-        } else {
+        } else if(status[0].code===285){
+          
+          if (lang === "fa") {
+            var text = "آدرس خود را وارد کنید";
+          } else {
+            var text = "Enter your Addres";
+          }
+          notify(text, "error");
+          setpreloadPay(false);
+        }else {
           if (lang === "fa") {
             var text = "خطایی رخ داده";
           } else {
