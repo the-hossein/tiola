dcfbd81bcb50d4db11e1b62ef36f595b5841e4ac
Erfan Hoseini <s.erfan.hoseini.81@gmail.com>
2022-06-08 19:42:24 +0430

    new edit

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -16,7 +16,7 @@ const ContactUs = () => {
     const {t} = useTranslation();
 
     return (
-        <div className={`${style.container}`}>
+        <div className={`${style.container} `}>
                 <div className={style.img}></div>
                 <div className={`${style.address}`}>
                     <h1>{t("ContactUs")}</h1>
@@ -26,8 +26,8 @@ const ContactUs = () => {
                 </div>
                 <div className={`${style.phone} `}>
                     <h1></h1>
-                    <p>021 88 288 206</p>
-                    <p>Phone 2</p>
+                    <p>021 26 420 420</p>
+                    <p>021 91 690 818</p>
                     <p>Phone 3</p>
                     <div className={style.logos}>
                         <TwitterIcon fontSize='large' />
